1f5fd9b859a4acc5659b2fb1aa0b0d6a31370b09
Awe Morris <awe@noctvm.io>
2026-07-03 05:57:15 +0900

    Update

--- Formula/suika3.rb
+++ Formula/suika3.rb
@@ -3,14 +3,16 @@ require 'rbconfig'
 class Suika3 < Formula
   desc "2D Game and Visual Novel Engine"
   homepage "https://github.com/awemorris/suika3"
-  url "https://github.com/awemorris/suika3/archive/refs/tags/26.07.3.zip"
-  sha256 "f8ea3fe6f831aa0ee775d30c8a38f8011641917e9b6bb2ad4f46c744832be157"
+
+  url "https://github.com/awemorris/suika3.git", revision: "2e512a1e7b8c609a51527a7f95a161933650ef58"
+  version "26.07.3"
+
   license "Zlib"
 
   depends_on "cmake" => :build
 
   def install
-    system "cmake", ".", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.11", "-DSUIKA_TARGET_MACOS_CLI=ON", "-DSUIKA_ENABLE_INSTALL=ON", "-DSUIKA_ENABLE_JIT=ON", "-DSUIKA_ENABLE_I18N=ON", "-DSUIKA_ENABLE_PACK=ON", "-DSUIKA_ENABLE_BYTECOMP=ON", "-DSUIKA_ENABLE_AOTCOMP=ON", "-DSUIKA_ENABLE_CONSOLE=ON", "-DSUIKA_ENABLE_UNSAFE=ON", *std_cmake_args
+    system "cmake", ".", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.11", "-DSUIKA_TARGET_MACOS_CLI=ON", "-DSUIKA_ENABLE_INSTALL=ON", "-DSUIKA_ENABLE_JIT=ON", "-DSUIKA_ENABLE_I18N=ON", "-DSUIKA_ENABLE_PACK=ON", "-DSUIKA_ENABLE_BYTECOMP=ON", "-DSUIKA_ENABLE_AOTCOMP=ON", "-DSUIKA_ENABLE_CONSOLE=ON", "-DSUIKA_ENABLE_DIST_SINGLE=ON", "-DSUIKA_ENABLE_UNSAFE=ON", *std_cmake_args
     system "cmake", "--build", "."
 
     bin.install "suika3"
